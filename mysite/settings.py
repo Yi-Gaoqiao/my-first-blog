a55438ecc005f46e281d39e52602f3244383411a
Tsubasa <2bsttas@gmail.com>
2020-04-16 19:42:43 +0900

    8th commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yse!e117*#&#=%23r44b-162@osu%fwr(3^p#$hnu70a*c4x6-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
