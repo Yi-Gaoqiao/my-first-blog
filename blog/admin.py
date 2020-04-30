cfa90da348524387554cb6c6341546909cbed5d7
Tsubasa <2bsttas@gmail.com>
2020-04-30 10:16:40 +0900

    add display

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from .models import Post
 
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_date', 'updated_date')
+    list_display_links = ('id', 'title')
+
 admin.site.register(Post)
